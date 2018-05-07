0336148203a13fa4f832c1dfd207c3422866efa4
OstapFilipenko <171wita23@o365.htl-leoben.at>
2018-05-07 14:34:55 +0200

    Fizzer v.3

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,18 +1,21 @@
 public class FizzBuzz {
 	public static void main(String[] args) {
-		for (int i = 1; i <= 100; i++) {
-			FizzBuzz(i);
+		//for (int i = 1; i <= 100; i++) {
+			//FizzBuzz(i);
+		//}
+		for(int i = 4; i<=100; i+=4) {
+			FizzBuzz(i, false);
 		}
 	}
 
-	public static void FizzBuzz(int i) {
+	public static void FizzBuzz(int i, boolean showFizzBuzz) {
 
-		if (i % 7 == 0 && i % 4 == 0) {
-			System.out.println("Fizzer!Buzzer!");
+		if (i % 7 == 0 && i % 4 == 0 && showFizzBuzz == true) {
+			System.out.println("Fizzer!Buzzer!(" + i +")");
 		} else if (i % 4 == 0) {
-			System.out.println("Fizzer!");
+			System.out.println("Fizzer!(" + i +")");
 		} else if (i % 7 == 0) {
-			System.out.println("Buzzer!");
+			System.out.println("Buzzer!(" + i + ")");
 		} else if (i % 4 != 0 && i % 7 != 0) {
 			System.out.println(i);
 		}
